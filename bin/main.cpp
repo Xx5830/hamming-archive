fc90a80b591073f4db4b9af28cf253669e572d37
X_xp <timurbilalov14@gmail.com>
2025-12-01 21:17:49 +0300

    clean

--- bin/main.cpp
+++ bin/main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char **argv) {
     }
     if (parser.erase) {
         for (uint32_t index = 0; index < parser.files.size(); index++) {
-            std::string file_name = parser.files[index];
+            std::string file_name = parser.files[index];;
             my_archive.Delete(file_name);
         }
     }
